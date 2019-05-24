0e1b91d989226ef09ec871e4633e7daa3c3f4c39
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2019-05-24 08:38:54 +0700

    dhlk

---
+++ app/src/main/java/com/example/simple_mvvm/ViewModel/ItemPeopleViewModel.java
@@ -0,0 +1,9 @@
+package com.example.simple_mvvm.ViewModel;
+
+import android.databinding.BaseObservable;
+
+import com.example.simple_mvvm.model.People;
+
+public class ItemPeopleViewModel extends BaseObservable {
+    private People people;
+}
